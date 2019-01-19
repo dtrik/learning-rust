299fcc27582992869e1d949c4981d8c6dd0802fc
dtrik <damunano.1989@gmail.com>
2019-01-19 16:23:05 +0530

    First example of ownership with strings

---
+++ ownership_1/target/debug/deps/ownership_1-165a610409fe7908.d
@@ -0,0 +1,5 @@
+/home/damu/projects/learning-rust/ownership_1/target/debug/deps/ownership_1-165a610409fe7908: src/main.rs
+
+/home/damu/projects/learning-rust/ownership_1/target/debug/deps/ownership_1-165a610409fe7908.d: src/main.rs
+
+src/main.rs:
---
+++ ownership_1/target/debug/ownership_1.d
@@ -0,0 +1,1 @@
+/home/damu/projects/learning-rust/ownership_1/target/debug/ownership_1: /home/damu/projects/learning-rust/ownership_1/src/main.rs
