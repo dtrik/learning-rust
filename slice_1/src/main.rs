f66ef011e21025a8d6575ef85e5a38d0f75eb78c
dtrik <damunano.1989@gmail.com>
2019-01-21 00:19:24 +0530

    added implementation of first word based on slicing

--- slice_1/src/main.rs
+++ slice_1/src/main.rs
@@ -1,14 +1,17 @@
 fn main() {
     let mut s = String::from("hello world");
-    let word = first_word(&s);
+    let word = first_word_impl1(&s);
     println!("s is {}", s);
     println!("word is {}", word);
-    let first_word = &s[0..word];               //borrow slice of s
-    println!("first_word is {}", first_word); 
+    let first_word_1 = &s[0..word];         //borrow slice of s
+    println!("first_word is {}", first_word_1); 
+    let first_word_2 = first_word_impl2(&s);
+    println!("Another implementaton of first word");
+    println!("first word is still {}", first_word_2);
     s.clear();
 }
 
-fn first_word(s: &String) -> usize {
+fn first_word_impl1(s: &String) -> usize {
     let bytes = s.as_bytes();                   //convert s to bytes
     //iter returns each element in collection, enumerate returns each element as
     //part of tuple = (index, reference)
@@ -20,3 +23,12 @@ fn first_word(s: &String) -> usize {
     s.len()
 }
 
+fn first_word_impl2(s: &String) -> &str {
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[0..i]
+        }
+    }
+    &s
+}
