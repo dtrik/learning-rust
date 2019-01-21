bc42ff6b6f8e9707b25960d8b0266a9470467e14
dtrik <damunano.1989@gmail.com>
2019-01-21 23:22:45 +0530

    moving struct to inside main

--- struct_1/src/main.rs
+++ struct_1/src/main.rs
@@ -1,11 +1,12 @@
-struct User {
+
+fn main() {
+    struct User {
     username: String,
     email: String,
     sign_in_count: u64,
     active: bool,
-}
+    }
 
-fn main() {
     let mut user1 = User {
         email: String::from("someone@example.com"),
         username: String::from("someusername123"),
