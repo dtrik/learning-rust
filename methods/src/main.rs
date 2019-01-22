a3d93a44ce9c6de6f8295bc1e448bdc00fb3c8c9
dtrik <damunano.1989@gmail.com>
2019-01-23 00:41:00 +0530

    added associated function to create a square using rectangle struct

--- methods/src/main.rs
+++ methods/src/main.rs
@@ -17,13 +17,16 @@ impl Rectangle {
         self.width > other_rect.width && self.height > other_rect.height
     }
 
+    fn square(size: u32) -> Rectangle {
+        Rectangle {width: size, height: size}
+    }
 }
 
 fn main() {
     let rect1 = Rectangle {width: 30, height: 50};
     let rect2 = Rectangle {width: 10, height: 40};
     let rect3 = Rectangle {width: 60, height: 45};
-
+    let sq1 = Rectangle::square(30);
     println!(
         "Area of the rectangle is {} square pixels",
         rect1.area()
@@ -36,4 +39,5 @@ fn main() {
 
    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+   println!("Created {:?}", sq1);
 }
