ade45c88ce422a4a76982687ddf0d599beb1a5bf
dtrik <damunano.1989@gmail.com>
2019-01-22 23:49:31 +0530

    added option to print struct

--- struct_6/src/main.rs
+++ struct_6/src/main.rs
@@ -1,7 +1,9 @@
 fn main() {
+    #[derive(Debug)]
     struct Color(i32, i32, i32);
+    #[derive(Debug)]
     struct flag(Color);
 
     let india = flag(Color(0,0,0));
-    println!("India's flag color is {:?}", (india.0).0);
+    println!("India's flag has {:?}", india.0);
 }
