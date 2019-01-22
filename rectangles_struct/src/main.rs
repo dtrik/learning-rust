5ea4b3a41421e3a1f4637af9892ea7dd5ad4f7db
dtrik <damunano.1989@gmail.com>
2019-01-22 23:46:16 +0530

    added printing option for struct

--- rectangles_struct/src/main.rs
+++ rectangles_struct/src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Debug)]
 struct Rectangle {
     width: u32,
     height: u32,
@@ -6,7 +7,8 @@ struct Rectangle {
 fn main() {
     let rect1 = Rectangle { width: 30, height: 50 };
     println!(
-        "The area of the rectangle is {} square pixels",
+        "The area of the {:?}, is {} square pixels",
+        rect1,
         area(&rect1)
         );
 }
