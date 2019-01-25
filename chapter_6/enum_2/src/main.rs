30c0973f2b1f6dc60256a1c56cff23f93f786e77
dtrik <damunano.1989@gmail.com>
2019-01-26 03:01:13 +0530

    adding comments on enum_2

--- chapter_6/enum_2/src/main.rs
+++ chapter_6/enum_2/src/main.rs
@@ -1,15 +1,15 @@
 #![allow(unused_variables)]
 fn main() {
-    enum Message {
+    enum Message { //all variants of different type
         Quit,
         Move { x : i32, y: i32 },
         Write(String),
         ChangeColor(i32, i32, i32),
     }
-    impl Message {
-        fn call(&self) {
+    impl Message { //implement a method on enum
+        fn call(&self) { //method can be used on enum created
         }
     }
     let m = Message::Write(String::from("hello"));
-    m.call();
+    m.call(); //method being called on enum
 }
