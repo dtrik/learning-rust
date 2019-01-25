8f3e75cebd6b2ce4f9f04a9847f3bdd952b4f16f
dtrik <damunano.1989@gmail.com>
2019-01-26 01:53:12 +0530

    added comments

--- chapter_6/enum_1/src/main.rs
+++ chapter_6/enum_1/src/main.rs
@@ -1,17 +1,26 @@
 #![allow(unused_variables)]
 fn main() {
-
+   
+    #[derive(Debug)] 
+    //basic enum definition, enumerate all options
     enum IpAddrBasic {
         V4,
         V6,
     }
 
+    //define two variables using the :: syntax
+    let four = IpAddrBasic::V4;
+    let six = IpAddrBasic::V6;
+	
+    //create a more complex datatype that refers to above enum
+    #[derive(Debug)]
     struct IpAddrStruct {
-        kind: IpAddrBasic,
+        kind: IpAddrBasic,              //enum used as type of kind
         address: String,
     }
-    
-    let home_struct = IpAddrStruct {
+
+    //two variable of type struct defined above
+    let home_struct = IpAddrStruct {    
         kind: IpAddrBasic::V4,
         address: String::from("127.0.0.1"),
     };
@@ -21,8 +30,19 @@ fn main() {
         address: String::from("::1"),
     };
     
- 
-    enum IpAddrKind {
+    println!("{:?}", home_struct);
+    //function that takes data of type enum
+    //note it is generic, it can take either type of IP
+    fn route(ip_type: IpAddrBasic) {
+    //any process, current noop
+    }
+    
+    //call above function with both kinds
+    route(IpAddrBasic::V4);
+    route(IpAddrBasic::V6);
+	
+    //implement same operation as struct with simpler notation
+    enum IpAddrKind { //below says both V4 and V6 have assoc. string vals
         V4(String),
         V6(String),
     }
@@ -30,11 +50,4 @@ fn main() {
     let home_enum = IpAddrKind::V4(String::from("127.0.0.1"));
     let loopback_enum = IpAddrKind::V6(String::from("::1"));
    
-    fn route(ip_type: IpAddrBasic) {}
-    route(IpAddrBasic::V4);
-    route(IpAddrBasic::V6);
-	
-    let four = IpAddrBasic::V4;
-    let six = IpAddrBasic::V6;
-	
 }
