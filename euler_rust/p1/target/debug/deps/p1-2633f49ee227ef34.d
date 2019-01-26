519af4753bb6146a762d7e5019748ad54f66caf9
dtrik <damunano.1989@gmail.com>
2019-01-26 13:50:55 +0530

    removing wrong files

--- euler_rust/p1/target/debug/deps/p1-2633f49ee227ef34.d
+++
@@ -1,5 +0,0 @@
-/home/damu/projects/learning-rust/euler/p1/target/debug/deps/p1-2633f49ee227ef34.rmeta: src/main.rs
-
-/home/damu/projects/learning-rust/euler/p1/target/debug/deps/p1-2633f49ee227ef34.d: src/main.rs
-
-src/main.rs:
