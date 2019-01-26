89a4f6709cff3ff2cea279908d4e0949cfcb1e52
dtrik <damunano.1989@gmail.com>
2019-01-26 15:50:21 +0530

    project euler in rust

--- euler_rust/p1/target/debug/deps/p1-241e7e3490a7c6a2.d
+++
@@ -1,5 +0,0 @@
-/home/damu/projects/learning-rust/euler/p1/target/debug/deps/p1-241e7e3490a7c6a2: src/main.rs
-
-/home/damu/projects/learning-rust/euler/p1/target/debug/deps/p1-241e7e3490a7c6a2.d: src/main.rs
-
-src/main.rs:
--- euler_rust/p1/target/debug/p1.d
+++
@@ -1,1 +0,0 @@
-/home/damu/projects/learning-rust/euler/p1/target/debug/p1: /home/damu/projects/learning-rust/euler/p1/src/main.rs
