9dc78c84c56148187760456cc46b9b939c2c38ec
dtrik <damunano.1989@gmail.com>
2019-01-27 23:49:13 +0530

    adding info in variant, used in function

--- chapter_6/match_1/src/main.rs
+++ chapter_6/match_1/src/main.rs
@@ -1,11 +1,17 @@
 #![allow(unused_variables)]
 fn main() {
     #[derive(Debug)]
+    enum UsState {
+        Alaska,
+        Alabama,
+    }
+
+    #[derive(Debug)]
     enum Coin {
         Penny,
         Nickel,
         Dime,
-        Quarter,
+        Quarter(UsState),
     }
     fn value_in_cents(coin: Coin) -> u32 {
         match coin {
@@ -15,10 +21,15 @@ fn main() {
             },
             Coin::Nickel => 5,
             Coin::Dime => 10,
-            Coin::Quarter => 25,
+            Coin::Quarter(state) => {
+                println!("Minted for {:?}", state);
+                25
+            },
         }
     }
-    let coin_test = Coin::Penny;
+    let coin_penny = Coin::Penny;
+    let coin_quarter_alaska = Coin::Quarter(UsState::Alaska);
 
-    println!("Value of coin_test is {:?}",  value_in_cents(coin_test));
+    println!("Value of coin_test is {:?}",  value_in_cents(coin_penny));
+    println!("Coin is worth {:?} cents", value_in_cents(coin_quarter_alaska));
 }
